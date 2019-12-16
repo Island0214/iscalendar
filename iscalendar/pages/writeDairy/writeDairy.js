02d5e89f667e32e08af4d91ff34a39d06b1a7de2
Y-dc <yuan1121252032@163.com>
2019-12-16 15:22:39 +0800

    <fix>:<fix add image into dairy>

--- iscalendar/pages/writeDairy/writeDairy.js
+++ iscalendar/pages/writeDairy/writeDairy.js
@@ -157,37 +157,6 @@ Page({   //页面的生命周期钩子、事件处理函数、页面的默认数
     })
   },
 
-  addImage() {
-    const that = this;
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success: function (res) {
-        console.log(res.tempFilePaths,'上传图片')
-        wx.uploadFile({
-          url: '自己的图片上传地址',
-          filePath: res.tempFilePaths[0],
-          name: 'file',
-          formData: {
-            app_token: app.data.userInfo.app_token,
-          },
-          success: function (res) {
-            console.log(res.data,'图片上传之后的数据')
-            var data = JSON.parse(res.data)
-            console.log(data.data.url)
-            that.editorCtx.insertImage({
-              src: data.data.url,
-              success: function () {
-                console.log('insert image success')
-              }
-            })
-          }
-        }) 
-      }
-    })
-  },
-
   returnLastPage:function(){
     wx.navigateBack()
   }
