fa6fd1284dc196a8d24533e20ab871127f0b4204
Island0214 <549378813@qq.com>
2019-12-21 04:44:17 +0800

    <feat>: <add Login Page>

--- iscalendar/app.js
+++ iscalendar/app.js
@@ -13,14 +13,16 @@ App({
         
       }
     })
+
     // 获取用户信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+        //   // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
+              console.log(res.userInfo)
               this.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
@@ -34,6 +36,8 @@ App({
       }
     })
 
+    // this.login()
+
     var day = new Date();
     day.setTime(day.getTime());
     var arr_week = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
@@ -45,7 +49,7 @@ App({
       'week': arr_week[day.getDay()]
     }
   },
-  getFormatDate: function (str) {
+  getFormatDate: function(str) {
     var arr_week = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat');
     var arr_month = new Array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
     var day = new Date(str);
@@ -60,5 +64,49 @@ App({
   globalData: {
     userInfo: null,
     today: {}
-  }
+  },
+  login() {
+    wx.login({
+      success: res => {
+        console.log(res)
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        if (res.code) {
+          // wx.reLaunch({
+          //   url: '/pages/login/login',
+          // })
+          wx.request({
+            url: 'http://fa.com/api/schoolreserve/login',
+            data: {
+              code: res.code,
+              user_info: this.globalData.userInfo
+            },
+            success: function (res) {
+              console.log('回调成功')
+              console.log(res.data.data)
+              wx.setStorageSync('token', res.data.data.token)
+              wx.setStorageSync('user_id', res.data.data.user_id)
+            },
+            complete: function () {
+              wx.checkSession({
+                success() {
+                  console.log('经过验证，登录有效')
+                  // session_key 未过期，并且在本生命周期一直有效
+                },
+                fail() {
+                  console.log('session过期，请重新登录')
+                  // session_key 已经失效，需要重新执行登录流程
+                  wx.switchTab({
+                    url: '/pages/login/login',
+                  })
+                }
+              })
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+
+      }
+    })
+  },
 })
